a0148a01561e504392e2f4c86fbada434ba1e14c
Luizhenrique0001 <rodriguesluizhenrique316@gmail.com>
2025-09-12 09:47:19 -0300

    Pequena mudança para typeScript

---
+++ src/Components/FoodCard.tsx
@@ -0,0 +1,18 @@
+interface CardProps {
+    title: string
+    img: string
+    price: number
+}
+
+function FoodCard({img, title, price}: CardProps) {
+    return (
+        <div className="px-5">
+            <div className="bg-slate-500 h-70 rounded-lg text-white items-center">
+                <img className="max-w-25" src={img} alt="" />
+                <h1 className="text-xl font-bold">{title}</h1>
+                <p><b>Valor: </b> {price}</p>
+            </div>
+        </div>
+    );
+}
+export default FoodCard;
